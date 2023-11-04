8db8ee9b10067db7eb058e1238a5d83f21d9a61d
Aavishkar <ag8298@nyu.edu>
2023-11-04 17:09:23 -0400

    add hangman

---
+++ src/pygamebox/hangman.py
@@ -0,0 +1,56 @@
+import requests
+class Hangman():
+    def __init__(self, length):
+        self.word = self.get_word(length)
+        self.guesses = 0
+        self.reset(length)
+
+    def reset(self, length):
+        self.word = self.get_word(length)
+        self.guesses = 0
+
+    def get_word(self, length):
+        #returns words from a 
+        word = requests.get(f"https://random-word-api.herokuapp.com/word?length={length}").json()[0]
+        return word
+    
+    def get_user_guess(self):
+        return input('Guess a letter: ')
+    
+    def validate_input(self, guess):
+        if len(guess) != 1:
+            print('Please enter a single letter')
+            return False
+        return True
+    
+    def check_win(self, predict):
+        for letter in predict:
+            if letter == '_':
+                return False
+        return True
+        
+    def play(self):
+        print('---Guess the 5 letter word!---')
+        print(self.word)
+        predict = ['_' for i in range(len(self.word))]
+        while self.guesses < 5:
+            print(f'You have {5 - self.guesses} guesses left')
+            print()
+            print( predict )
+            guess = self.get_user_guess()
+            if self.validate_input(guess):
+                if guess in self.word:
+                    indices = [i for i in range(len(self.word)) if self.word[i] == guess]
+                    for i in indices:
+                        predict[i] = guess
+                    if self.check_win(predict):
+                        print('You win!')
+                        return True
+                else:
+                    print('Incorrect!')
+                    self.guesses += 1
+        print('You lose!')
+        return False
+    
+word = Hangman(4)
+word.play()
---
+++ tests/test_hangman.py
@@ -0,0 +1,30 @@
+from pygamebox import Hangman
+
+def test_get_word():
+    length = 2
+    hangman = Hangman()
+    assert len(hangman.word) == length
+    assert hangman.guesses == 0
+
+def get_user_guess():
+    hangman = Hangman()
+    assert hangman.get_user_guess() == 'a'
+
+def test_validate_input():
+    hangman = Hangman()
+    assert hangman.validate_input('a') == True
+    assert hangman.validate_input('aa') == False
+    assert hangman.validate_input('') == False
+
+def test_check_win():
+    hangman = Hangman()
+    assert hangman.check_win(['a', 'a', 'a', 'a']) == True
+    assert hangman.check_win(['a', 'a', '_', 'a']) == False
+
+def test_play():
+    hangman = Hangman()
+    
+
+    pass
+
+
