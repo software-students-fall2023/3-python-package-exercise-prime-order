8e4b82008674b0c1b7bc9b8081393e5f25b8ee14
Aavishkar <ag8298@nyu.edu>
2023-11-04 17:51:29 -0400

    fixed changes for hangman

--- src/pygamebox/__init__.py
+++ src/pygamebox/__init__.py
@@ -1,3 +1,4 @@
 from .guess_the_number import GuessTheNumber
 from .rock_paper_scissors import RockPaperScissors
 from .word_scramble import WordScramble
+from .hangman import Hangman
--- tests/test_hangman.py
+++ tests/test_hangman.py
@@ -2,27 +2,27 @@ from pygamebox import Hangman
 
 def test_get_word():
     length = 2
-    hangman = Hangman()
+    hangman = Hangman(length)
     assert len(hangman.word) == length
     assert hangman.guesses == 0
 
 def get_user_guess():
-    hangman = Hangman()
+    hangman = Hangman(4)
     assert hangman.get_user_guess() == 'a'
 
 def test_validate_input():
-    hangman = Hangman()
+    hangman = Hangman(5)
     assert hangman.validate_input('a') == True
     assert hangman.validate_input('aa') == False
     assert hangman.validate_input('') == False
 
 def test_check_win():
-    hangman = Hangman()
+    hangman = Hangman(3)
     assert hangman.check_win(['a', 'a', 'a', 'a']) == True
     assert hangman.check_win(['a', 'a', '_', 'a']) == False
 
 def test_play():
-    hangman = Hangman()
+    hangman = Hangman(4)
     pass
 
 
